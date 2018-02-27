526056657e42e8ca06905f0aaf58e7aca97a2358
城　健太郎 <ks19790209@gmai.com>
2018-02-27 14:57:55 +0900

    db作成

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require 'bundler'
 
 Bundler.require
 set :database,{adapter: "sqlite3",database: "datas.sqlite3"}
+class Datas < ActiveRecord::Base
+  validates_presence_of :area
+  validates_presence_of :name
+  validates_presence_of :adress
+  validates_presence_of :email
+  validates_presence_of :tel
 
 get '/' do
  erb :index_staff_all
@@ -17,5 +23,9 @@ end
 post '/datas' do
  puts "###これはPOSTされたデータです###"
  p params
+ name=params[:area,:name,:adress,:email,:tel]
+ data=Datas.new({area: area,name: name,adress: adress,email: email,tel: tel })
+ data.save
  redirect '/'
+ end
 end
--- db/migrate/20180224075004_create_datas.rb
+++ db/migrate/20180224075004_create_datas.rb
@@ -1,4 +1,10 @@
 class CreateDatas < ActiveRecord::Migration[5.1]
   def change
+    create_table :datas do |t|
+      t.string :area
+      t.string :name
+      t.string :adress
+      t.string :email
+      t.string :tel
   end
 end
